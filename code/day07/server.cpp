afcf9243ad1e591a18b256ad7a133b06437af616
qjl <jiale_quan@ustc.edu>
2023-01-06 10:53:13 +0800

    delete

--- code/day07/server.cpp
+++
@@ -1,9 +0,0 @@
-#include "src/eventLoop.h"
-#include "src/server.h"
-
-int main() {
-    EventLoop *loop = new EventLoop();
-    Server *server = new Server(loop);
-    loop->loop();
-    return 0;
-}
